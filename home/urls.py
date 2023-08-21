938cbe5ca1ac4c53bd0dee5fca06c18c1ca5f252
lenis <fardintorkamand83@gmail.com>
2023-08-21 18:56:11 +0330

    add navbar and some fronted change, implement detail view for Todo model

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-
+    path('', views.home, name='home'),
+    path('detail/<int:todo_id>/', views.detail, name='detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ from .models import Todo
 def home(request):
     todos = Todo.objects.all()
     return render(request, 'home.html', context={'todos': todos})
+
+
+def detail(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    return render(request, 'detail.html', context={'todo': todo})
