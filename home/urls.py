813f4052eb439e58ed1c2db1fa21d80d86f7437d
lenis <fardintorkamand83@gmail.com>
2023-08-21 19:46:07 +0330

    implement delete view for todo

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home_page'),
     path('detail/<int:todo_id>/', views.detail, name='detail'),
+    path('delete/<int:todo_id>/', views.delete, name='delete'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 
 
@@ -10,3 +10,8 @@ def home(request):
 def detail(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     return render(request, 'detail.html', context={'todo': todo})
+
+
+def delete(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return redirect('home:home_page')
