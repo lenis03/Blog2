e9503b7ec5d9aab6e604ab433dccdc5f4a9f3a1c
lenis <fardintorkamand83@gmail.com>
2023-08-21 19:01:38 +0330

    add app name for home

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home_page'),
     path('detail/<int:todo_id>/', views.detail, name='detail'),
 ]
