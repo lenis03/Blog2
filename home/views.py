c8fbec2b730c2c4645a9586f6b0acf62065f85a8
lenis <fardintorkamand83@gmail.com>
2023-08-22 14:34:09 +0330

    add message for delete view and change direction home and detail template

--- home/views.py
+++ home/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Todo
+from django.contrib import messages
 
 
 def home(request):
     todos = Todo.objects.all()
-    return render(request, 'home.html', context={'todos': todos})
+    return render(request, 'home/home.html', context={'todos': todos})
 
 
 def detail(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
-    return render(request, 'detail.html', context={'todo': todo})
+    return render(request, 'home/detail.html', context={'todo': todo})
 
 
 def delete(request, todo_id):
     Todo.objects.get(id=todo_id).delete()
+    messages.success(request, 'Todo deleted successfully', 'success')
     return redirect('home:home_page')
