0817a26a9e2be979004793687d3693a213fbb8d2
lenis <fardintorkamand83@gmail.com>
2023-08-23 14:42:19 +0330

    implement user login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 
 
 def user_register(request):
@@ -22,4 +23,29 @@ def user_register(request):
             return redirect('home:blog_list')
     else:
         form = UserRegistrationForm()
-    return render(request, 'accounts/register.html', {'form': form})          
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['user_name'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Logged in successfully.', 'success')
+                return redirect('home:blog_list')
+            else:
+                messages.error(request, 'Invalid username or password', 'danger')
+
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'logged out successfully')
+    return redirect('home:blog_list')
